fbc4daffa2d9e704181d6165180a0d1b79f0b0a7
arshiya-stem <arshiya.mostafavisabet@stemminds.com>
2026-03-23 01:41:28 -0400

    Delete Climate_Tree directory

--- Climate_Tree/Moisture_Sensor_API.cpp
+++
@@ -1,9 +0,0 @@
-#include "Moisture_Sensor_API.h"
-
-Moisture_Sensor_API::Moisture_Sensor_API(int analog_pin) : pin(analog_pin) {}
-
-float Moisture_Sensor_API::ReadMoisture(){
-  return (analogRead(pin) / 1023.0) * 100;
-}
-
-
--- Climate_Tree/Ph_Sensor_API.cpp
+++
@@ -1,7 +0,0 @@
-#include "Ph_Sensor_API.h"
-
-Ph_Sensor_API::Ph_Sensor_API(int analog_pin) : pin(analog_pin) {}
-
-float Ph_Sensor_API::ReadPH() {
-  return  (7 - 1000.0 * (analogRead(pin) - 1440) * 3.3 / 59.16 / 4095.0);
-}
